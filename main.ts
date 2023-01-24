2a91abab9a32a0968cf2ae8f9775f20185d14212
UJGunner <122560296+UJGunner@users.noreply.github.com>
2023-01-24 10:08:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,13 +8,47 @@ input.onButtonPressed(Button.B, function () {
     lives += -1
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    radio.sendString("urijah's score")
     radio.sendString("" + (score))
+    basic.showString("score")
+    basic.showString("" + (score))
 })
+let high_score = 0
 let score = 0
 let lives = 3
 basic.forever(function () {
-    if (lives == 0) {
+    if (lives <= 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showString("game over")
+        basic.showString("score")
+        basic.showString("" + (score))
+    }
+})
+basic.forever(function () {
+    if (lives <= 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
         radio.sendString("urijah's score")
         radio.sendString("" + (score))
+    }
+})
+basic.forever(function () {
+    if (high_score < score) {
+        high_score += score
+        if (lives <= 0) {
+            basic.showString("new high score")
+            basic.showString("" + (high_score))
+        }
     }
 })
